674ce22ab6f2fcd6294ff8ef378ba3318f798a27
Nina Yao <nini88518@gmail.com>
2024-07-09 16:27:14 +0800

    week-2 optional assignment

--- week-4/assignment-1&2/script.js
+++ week-4/assignment-1&2/script.js
@@ -31,3 +31,17 @@ const url =
 ajax(url).then((data) => {
   render(data);
 });
+
+//week-2 assignment5
+function twoSum(nums, target) {
+	for (let i=0; i < nums.length; i++){
+    for (let j=0; j < nums.length; j++){
+    if(nums[i]+nums[j] === target){
+      console.log([i, j]);
+      return[i,j];
+    }
+  }
+}
+return null
+}
+twoSum([2, 7, 11, 15], 9);
