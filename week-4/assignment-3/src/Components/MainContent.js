c56dc46813215cf46f9906dbfa662cdebd6f1cf9
Nina Yao <nini88518@gmail.com>
2024-07-12 16:51:22 +0800

    adjust assignment-3

--- week-4/assignment-3/src/Components/MainContent.js
+++ week-4/assignment-3/src/Components/MainContent.js
@@ -1,23 +1,39 @@
-import React from "react";
+import React from 'react';
 
-const MainContent = ({ handleCTAClick, boxDisplay }) => {
-  return (
-    <main className="main-content">
-      <section className="section">Section Title</section>
-      <div className="grid-container">
-        <div className="box">Content Box 1</div>
-        <div className="box">Content Box 2</div>
-        <div className="box">Content Box 3</div>
-        <div className="box">Content Box 4</div>
+const MainContent = ({ handleCTAClick, boxDisplay }) =>{
+  const generateContentBox = (boxes) => {
+    return boxes.map((content, index) => (
+      <div key={index} className="box">
+        {content}
       </div>
-      <div className="btn" onClick={handleCTAClick}>
+    ));
+  };
+
+  const contentBox = [
+    "Content Box 1",
+    "Content Box 2",
+    "Content Box 3",
+    "Content Box 4",
+  ];
+
+  const hiddenBoxes = [
+    "Content Box 5",
+    "Content Box 6",
+    "Content Box 7",
+    "Content Box 8",
+  ];
+
+  return (
+    <main className='main-content'>
+      <section className="section">Section Title</section>
+      <div className='grid-container'>
+        {generateContentBox(contentBox)}
+      </div>
+      <div className='btn' onClick={handleCTAClick}>
         Call to Action
       </div>
-      <div className="hidden-box" style={{ display: boxDisplay }}>
-        <div className="box">Content Box 5</div>
-        <div className="box">Content Box 6</div>
-        <div className="box">Content Box 7</div>
-        <div className="box">Content Box 8</div>
+      <div className='hidden-box' style={ {display: boxDisplay} }>
+        {generateContentBox(hiddenBoxes)}
       </div>
     </main>
   );
